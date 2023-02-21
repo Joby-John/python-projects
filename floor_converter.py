214bbaa970bf7f7d30c9fcc05ee7154c890a832e
Joby-John <joby4324@gmail.com>
2023-02-21 16:43:08 +0530

    modified:   floor_converter.py

--- floor_converter.py
+++ floor_converter.py
@@ -1,5 +1,5 @@
 print(" Welcome to lift converter ")
-euf = int(input("Enter the EU floor: "))
+euf = int(input("Enter the floor no you want to conert from EU to US: "))
 usf = euf+1 
 print( "That'll be floor number "+ str(usf) +" in US")
 print("thanks for using")
