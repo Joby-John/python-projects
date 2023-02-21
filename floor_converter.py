e7d94d337533e3cd8e0eb2045d651fc208f7cd1e
Joby-John <joby4324@gmail.com>
2023-02-21 17:43:08 +0530

    added try and except to catch errors

--- floor_converter.py
+++ floor_converter.py
@@ -1,5 +1,10 @@
 print(" Welcome to lift converter ")
-euf = int(input("Enter the floor no you want to conert from EU to US: "))
-usf = euf+1 
-print( "That'll be floor number "+ str(usf) +" in US")
-print("thanks for using")
+euf = (input("Enter the floor no you want to conert from EU to US: "))
+try:
+    euf = int(euf)
+    usf = euf+1 
+    print( "That'll be floor number "+ str(usf) +" in US")
+    print("thanks for using")
+except:
+    print( "Enter a number please")
+
