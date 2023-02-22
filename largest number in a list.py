5081f73a4ecd8bfbbab3a3e052b3f399371da208
Joby-John <joby432john@gmail.com>
2023-02-22 12:32:02 +0530

    changed variable names

--- largest number in a list.py
+++ largest number in a list.py
@@ -1,7 +1,7 @@
 # largest number in a list
 largest = -1
-for i in [25,12,85,32,5,75,37]:
-    if i>largest:
-        largest = i
+for pr_num in [25,12,85,32,5,75,37]:
+    if pr_num > largest:
+        largest = pr_num
 
 print('largest: ', largest)        
