afcb1898abd2e79b3151ff909605a652c259b1d6
Joby-John <joby432john@gmail.com>
2023-02-22 13:10:55 +0530

    used none  and is statements to find least & large

--- largest number in a list.py
+++ largest number in a list.py
@@ -1,15 +1,21 @@
 # largest number in a list containing numbers below 100
-largest = -1
-least = 100
+largest = None
+least = None # none keyword used for indicating nothing
 count = 0
 sum = 0
 for pr_num in [25,12,85,32,5,75,37]:
     count = count+1
     sum = sum+pr_num
-    if pr_num > largest:
+    if largest is None:
         largest = pr_num
+    elif pr_num > largest:
+        largest = pr_num
+            
+    #least conditions    
+    elif least is None: # is a keyword that's used to compare with none
+        least = pr_num 
     elif pr_num < least:
-        least = pr_num    
+        least = pr_num   
 
 print('largest: ', largest)
 print('least:', least)
