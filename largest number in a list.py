7eb9dfe7405f4b476e8bdd9cbe912036468e498c
Joby-John <joby432john@gmail.com>
2023-02-22 13:11:39 +0530

    modified largest number in a list.py

--- largest number in a list.py
+++ largest number in a list.py
@@ -3,14 +3,14 @@ largest = None
 least = None # none keyword used for indicating nothing
 count = 0
 sum = 0
-for pr_num in [25,12,85,32,5,75,37]:
+for pr_num in [25,12,85,32,-5,75,37]:
     count = count+1
     sum = sum+pr_num
     if largest is None:
         largest = pr_num
     elif pr_num > largest:
         largest = pr_num
-            
+
     #least conditions    
     elif least is None: # is a keyword that's used to compare with none
         least = pr_num 
