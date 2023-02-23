9487177588f9960bb23271241d273287534f959c
Joby-John <joby432john@gmail.com>
2023-02-23 23:36:44 +0530

    included close and seek functions

--- starting_files/file_reader.py
+++ starting_files/file_reader.py
@@ -6,6 +6,18 @@ except:
     quit() # quits the program
 
 
-count = 1
+count = 0
 for lines in fhand:
     print(lines)
+    count = count +1
+print('Number of lines:', count)
+fhand.seek(0) # sets the fhand pointer back to 0 ie, starting of the file else the next loop wont work
+
+c = 0
+for line in fhand:
+    c = c+1
+    if line.startswith('F'):
+        print('Lines starting with F: ', line)
+        print('line no:', c)
+
+fhand.close() # releases the resource in fhand and saves changes made        
