9c5b9ea123f29960e22c036cbadb2c678fd6b3b3
Joby-John <joby432john@gmail.com>
2023-02-28 18:49:15 +0530

    splitted further to display names

--- Email_data/searching.py
+++ Email_data/searching.py
@@ -1,4 +1,4 @@
-fhand = open('D:\projects\python-projects\Email_data\mbox-short.txt')
+fhand = open('Email_data\mbox-short.txt')
 #whole = fhand.read()
 
 for line in fhand:
@@ -6,5 +6,8 @@ for line in fhand:
     if not line.startswith('From'):
         continue
     sender = line.split()
-    print(sender[1])
+    #print(sender[1])      # prints the entire mail adress
+    mail = sender[1] # store the mail adress to mail
+    mparts = mail.split('@') # split mail address and stores in mparts
+    print(mparts[0]) # prints out name from mail address, whic his in index 0
 
