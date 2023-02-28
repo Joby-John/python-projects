64064e80fd4de3bd085ce91515fea6c7088fbe74
Joby-John <joby432john@gmail.com>
2023-02-28 11:54:53 +0530

    Average using list and list methods

--- lists2.py
+++ lists2.py
@@ -1,1 +1,13 @@
-print('hi')
+numbers = list()
+
+while True:
+    inp = input("Enter any number(one at a time), when done enter done:")
+    if inp == 'done':
+        break
+    value = float(inp)
+    numbers.append(value)
+try:    
+    Average = sum(numbers)/len(numbers)
+    print("Average: ", Average)
+except:
+    print("enter atleast one number!")
