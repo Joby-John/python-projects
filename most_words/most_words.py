7adf324bb971d052fa03918b4b10e76962e6ff48
Joby-John <joby432john@gmail.com>
2023-03-02 17:01:50 +0530

    finished most_words.py

--- most_words/most_words.py
+++ most_words/most_words.py
@@ -1,4 +1,23 @@
-print("There are two files, 'clowns.txt' and words.txt")
-inp = input("Enter a file name : ") 
-with open(inp) as file:
-    print(file) 
+print("There are two files, 'clown.txt' and words.txt")
+words = list()
+inp = input("Enter a file name : ")
+
+try: # to avoid getting error for invalid file name
+    with open(inp) as file:
+            f_cont = file.read()
+
+    for line in f_cont.splitlines(): # splitting the entire text to lines
+        for word in line.split(): # splitting lines to words
+                words.append(word) # storing those words as list
+
+    count = {}
+    for word in words:
+        count[word] = count.get(word, 0)+1
+
+    maximum = max(count, key = count.get)# max function to find the greatest value in dictionary
+
+    print("most occuring word :  "+ maximum + ", " + str(count[maximum]) + " times")      
+            
+
+except:
+     print("ERR! Enter a file that is present in the directory")        
