04d937f16a3dbbd36b80788339a63a8d50387129
Joby-John <joby432john@gmail.com>
2023-07-26 20:00:31 +0530

    implemented a condition to prevent fraud

--- closed_auction_dict/auction.py
+++ closed_auction_dict/auction.py
@@ -1,13 +1,21 @@
 import os
 import graphics
+import time
 
 print(graphics.logo)
 
 bid_list = {}
 while True:
     name_entry = input("Please enter your name :- ")
+    if name_entry in bid_list:   #to prevent others from modifying previously entered dictionary value
+        print("fraudulent duplicate entry detected and terminated, \n screen will be reactivated in 5s")
+        time.sleep(5) #used to freeze screen for 5seconds
+        print("The program continues now")
+        time.sleep(1)
+        continue
+        
     amount = input("Please enter the bid amount:- ")
-    bid_list[name_entry] = int(amount)
+    bid_list[name_entry] = int(amount) # this line stores amount in key name_entry variable which keeps changing in each iteration
     more = input("Is there more persons, Type 'yes' if yes else type 'no' :- " ).lower()
     if more == "no":
         os.system('CLS')
