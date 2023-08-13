b36b8f37928c65267a3075602847102202bc432c
Joby-John <joby432john@gmail.com>
2023-08-13 13:22:16 +0530

    added new game

---
+++ games/higher_lower/main.py
@@ -0,0 +1,60 @@
+from . import graphics
+import os
+from . import game_data
+import random
+import time
+
+data = game_data.data
+score = 0
+os.system('CLS')
+
+def first_comp():
+    """Getting first input"""
+    comp_a = random.choice(data)
+    print(f"Compare A: {comp_a['name']}, {comp_a['description']}, from {comp_a['country']}")
+    return comp_a
+
+def second_comp():
+    """Getting second input"""
+    comp_b = random.choice(data)
+    print(f"Compare B: {comp_b['name']}, {comp_b['description']}, from {comp_b['country']}")
+    return comp_b
+
+def logic(first_comp, second_comp):
+    """same as the name suggests"""
+    if first_comp['follower_count'] > second_comp['follower_count']: right = 'a'
+    elif second_comp['follower_count'] > first_comp['follower_count']: right = 'b'
+    else: right = "equal"
+
+    choice = input("\nWho has more followers A/B:- ")
+    if choice == right :
+        print("You are right!")
+        global score # because the score is defined globally else it'll get rest in every iteration
+        score += 1
+    else :
+        print(f"Sorry that's wrong, your total score :{score}")
+        return 0    
+
+def start():
+    while True:
+        print(graphics.logo)
+        comp_a = first_comp()
+        print(graphics.vs)
+        comp_b = second_comp()
+        cont = logic(comp_a, comp_b)
+        if cont == 0:
+            time.sleep(5)
+            if 'yes' == input("Do you want to play again yes/no:- "): 
+                global score
+                score = 0 #resetting score for new game
+            else:
+                score = 0     
+                break
+        else: 
+            time.sleep(3)
+            os.system('CLS')
+
+start()
+
+    
+
--- games/main.py
+++ games/main.py
@@ -2,20 +2,23 @@ from black_jack.main import startjack as start_black
 from hangman.hangman import start as start_hang
 from R_P_S import start as start_rock
 from number_guess.number_guess_game import game as number_guess
+from higher_lower.main import start as high_low
 import sys
 import os
+import graphics
 game = 0
 
-while game != 4:
+while True:
     os.system('CLS')
-    print("Welcome to Game Hub")
+    print(graphics.logo)
     print("Choices:")
-    print("1. Blackjack\n2. Hangman\n3. Rock Paper scissors\n4. Number Guess game\n5. Quit")
+    print("1. Blackjack\n2. Hangman\n3. Rock Paper scissors\n4. Number Guess game\n5. higher lower \n6. Quit")
     game = input("Enter the option you want to continue with:- ")
 
     if game == "1": start_black()
     elif game == "2": start_hang()
     elif game == "3": start_rock()
     elif game == "4": number_guess() 
-    elif game == "5":    sys.exit()
+    elif game == "5": high_low()
+    elif game == "6": sys.exit()
     else: print("Enter a valid option")
