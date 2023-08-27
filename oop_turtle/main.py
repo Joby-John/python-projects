5067ecc2cd81ec4510feeba4e12340a3072fd144
Joby-John <joby432john@gmail.com>
2023-08-27 23:31:16 +0530

    learning turtle and how installing new packages like pretty table, also learning about classes and oop

---
+++ oop_turtle/main.py
@@ -0,0 +1,25 @@
+from turtle import Turtle, Screen
+
+tony = Turtle()
+tony.shape('turtle')
+tony.color('darkolivegreen')
+tony.fd(100)
+tony.left(120)
+tony.fd(100)
+tony.left(120)
+tony.fd(100)
+tony.lt(120)
+
+my_screen = Screen()
+my_screen.exitonclick()
+
+# to install any packages from pypi just go to that site
+# find it
+# open hamburger menu in pycharm
+# go to settings
+# then project -> python interpreter
+# then click + in top and search for required package and then install
+# we can use it by importing as usual
+
+import prettytable
+
