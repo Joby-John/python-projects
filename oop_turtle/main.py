a68613fa35c46b27a57b60c81e51e610f7a7bf5e
Joby-John <joby432john@gmail.com>
2023-08-28 11:28:54 +0530

    how to access classes using objects, provide values, modify attributes and get desired results

--- oop_turtle/main.py
+++ oop_turtle/main.py
@@ -21,10 +21,11 @@ my_screen.exitonclick()
 # then click + in top and search for required package and then install
 # we can use it by importing as usual
 
-from prettytable import PrettyTable # importing the package class PrettyTable from package pretty table
+from prettytable import PrettyTable  # importing the package class PrettyTable from package pretty table
 
-table = PrettyTable()# assigning it to an object
+table = PrettyTable()  # assigning it to an object
 
-table.add_column("Pokemon", ["Pikachu", "Eevee", "Charizard"]) # accessing add_ column function from class
+table.add_column("Pokemon", ["Pikachu", "Eevee", "Charizard"])  # accessing add_ column function/method from class
 table.add_column("Power type", ["Electric", "Fire", "Fire"])
+table.align = "l"  # here the align is an attribute unlike add_colum which is a method , attributes are accessed like this
 print(table)
