a0a99a52a046f7d5b3c91106876e1c9a97c11a54
Joby-John <joby432john@gmail.com>
2023-09-05 23:58:49 +0530

    made a program that makes shapes by using turtles by accepting sides from user

---
+++ turtle_shapes/main.py
@@ -0,0 +1,22 @@
+import random
+import turtle
+from turtle import Turtle, Screen
+
+color_names = ["Red", "Green", "Blue", "Yellow", "Purple", "Orange", "Pink", "Brown", "Cyan", "Gray"]
+
+side = int(input("Enter side of the polygon you want to draw:- "))
+tony = Turtle()
+rony = Turtle()
+for i in range(3, side + 1):
+    start = i
+    tony.pencolor(random.choice(color_names))
+    rony.pencolor(random.choice(color_names))
+    for j in range(start):
+        tony.right(360 / start)
+        tony.fd(50)
+
+        rony.left(360 / start)
+        rony.fd(50)
+
+
+Screen().exitonclick()
