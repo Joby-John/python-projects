6b12256c27a93e3c0ffb30b6a9c517ca6cf01497
Joby-John <joby432john@gmail.com>
2023-09-06 00:52:53 +0530

    created a random walking turtle with setheading function that sets angle by anticlockwise

---
+++ random_walk_turtle/main.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+import random
+
+
+steps = int(input("Enter the steps you want to take:- "))
+tony = Turtle()
+tony.width(10)
+tony.speed(0)
+Screen().setup(600, 600)
+color_names = ["Red", "Green", "Blue", "Yellow", "Purple", "Orange", "Pink", "Brown", "Cyan", "Gray", "Black", "White", "Indigo", "Magenta", "Lavender", "Teal", "Maroon", "Turquoise", "Gold", "Silver"]
+
+for i in range(1, steps + 1):
+    angle = random.randint(0, 360)
+    color = random.choice(color_names)
+    tony.color(color)
+    tony.setheading(angle)
+    tony.forward(50)
+
+
+Screen().exitonclick()
