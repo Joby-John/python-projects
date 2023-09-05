b24307f51b7c818ba0ea00c64e2137696f243406
Joby-John <joby432john@gmail.com>
2023-09-06 00:12:26 +0530

    incorporated it in a function

--- turtle_shapes/main.py
+++ turtle_shapes/main.py
@@ -6,10 +6,9 @@ color_names = ["Red", "Green", "Blue", "Yellow", "Purple", "Orange", "Pink", "Br
 side = int(input("Enter side of the polygon you want to draw:- "))
 tony = Turtle()
 rony = Turtle()
-for i in range(3, side + 1):
-    start = i
-    tony.pencolor(random.choice(color_names))
-    rony.pencolor(random.choice(color_names))
+
+
+def draw(start):
     for j in range(start):
         tony.right(360 / start)
         tony.fd(50)
@@ -17,5 +16,11 @@ for i in range(3, side + 1):
         rony.left(360 / start)
         rony.fd(50)
 
+
+for i in range(3, side + 1):
+    start = i
+    tony.pencolor(random.choice(color_names))
+    rony.pencolor(random.choice(color_names))
+    draw(start)
 
 Screen().exitonclick()
