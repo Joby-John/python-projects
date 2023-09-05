472143ddf4bdd48a5cd8bc4a8873b2c719d66684
Joby-John <joby432john@gmail.com>
2023-09-05 08:59:23 +0530

    after square drawn dashed line

--- more_turtle/main.py
+++ more_turtle/main.py
@@ -4,9 +4,10 @@ tony = Turtle()
 scr = Screen()
 
 tony.shape("triangle")
-tony.color("wheat")
+tony.color("black")
 
 
+# square
 def square():
     tony.forward(100)
     tony.right(90)
@@ -14,5 +15,24 @@ def square():
 
 for i in range(4):
     square()
+
+# dashed line
+tony.penup()
+tony.left(180)
+tony.forward(500)
+tony.right(90)
+tony.forward(200)
+tony.right(90)
+
+
+def d_line():  # dashed line
+    tony.pendown()
+    tony.forward(10)
+    tony.penup()
+    tony.forward(10)
+
+
+for i in range(50):
+    d_line()
 
 scr.exitonclick()
