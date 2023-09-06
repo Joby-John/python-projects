b952466954bcf9a153e54f2cc157478a3bdabb5a
Joby-John <joby432john@gmail.com>
2023-09-06 11:26:00 +0530

    changed to turn only in right angles

--- random_walk_turtle/main.py
+++ random_walk_turtle/main.py
@@ -1,20 +1,19 @@
 from turtle import Turtle, Screen
 import random
 
-
 steps = int(input("Enter the steps you want to take:- "))
 tony = Turtle()
 tony.width(10)
-tony.speed(0)
+tony.speed(2)
 Screen().setup(600, 600)
-color_names = ["Red", "Green", "Blue", "Yellow", "Purple", "Orange", "Pink", "Brown", "Cyan", "Gray", "Black", "White", "Indigo", "Magenta", "Lavender", "Teal", "Maroon", "Turquoise", "Gold", "Silver"]
+color_names = ["Red", "Green", "Blue", "Yellow", "Purple", "Orange", "Pink", "Brown", "Cyan", "Gray", "Black", "White",
+               "Indigo", "Magenta", "Lavender", "Teal", "Maroon", "Turquoise", "Gold", "Silver"]
 
 for i in range(1, steps + 1):
-    angle = random.randint(0, 360)
+    angle = random.choice([90, 180, 270, 360])
     color = random.choice(color_names)
     tony.color(color)
     tony.setheading(angle)
-    tony.forward(50)
-
+    tony.forward(20)
 
 Screen().exitonclick()
