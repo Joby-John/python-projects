ddf8e3cb88dee66201058ac885a5080b80de7c0c
Joby-John <joby432john@gmail.com>
2023-09-06 12:53:41 +0530

    defined a functon to stop before overlappping, the program accepts an angle and draws accordingly without overlapping

--- spirograph/main.py
+++ spirograph/main.py
@@ -6,10 +6,17 @@ tony = Turtle()
 
 turtle.colormode(255)
 tony.speed("fastest")
-for i in range(100):
-    tony.color((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-    tony.circle(100)
-    curr_heading = tony.heading()  # to get the current angle of the turtle
-    tony.setheading(curr_heading + 5)  # adding 5 degrees to the previous angle
+angle = int(input("Enter the angle with which you want to draw, hint:- smaller = more beautiful:- "))
+
+
+def spirograph(angle_size):
+    for i in range(int(360 / angle_size)):  # bc circle can only be drawn a certain times without overlapping
+        tony.color((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        tony.circle(100)
+        curr_heading = tony.heading()  # to get the current angle of the turtle
+        tony.setheading(curr_heading + angle_size)  # adding 5 degrees to the previous angle
+
+
+spirograph(angle)
 
 Screen().exitonclick()
