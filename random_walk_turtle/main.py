34cdffa51fd7ec7da6a1749a6e54413c00ea207c
Joby-John <joby432john@gmail.com>
2023-09-06 12:08:29 +0530

    changed color mode of the module with turtle.colormode and given a tuple to make it really work with random colors

--- random_walk_turtle/main.py
+++ random_walk_turtle/main.py
@@ -1,18 +1,21 @@
+import turtle
 from turtle import Turtle, Screen
 import random
+
 
 steps = int(input("Enter the steps you want to take:- "))
 tony = Turtle()
 tony.width(10)
 tony.speed(2)
 Screen().setup(600, 600)
-color_names = ["Red", "Green", "Blue", "Yellow", "Purple", "Orange", "Pink", "Brown", "Cyan", "Gray", "Black", "White",
-               "Indigo", "Magenta", "Lavender", "Teal", "Maroon", "Turquoise", "Gold", "Silver"]
+turtle.colormode(255)
+#color_names = ["Red", "Green", "Blue", "Yellow", "Purple", "Orange", "Pink", "Brown", "Cyan", "Gray", "Black", "White",
+               #"Indigo", "Magenta", "Lavender", "Teal", "Maroon", "Turquoise", "Gold", "Silver"]
 
 for i in range(1, steps + 1):
     angle = random.choice([90, 180, 270, 360])
-    color = random.choice(color_names)
-    tony.color(color)
+    #color = random.choice(color_names)
+    tony.color((random.randint(0,255), random.randint(0,255), random.randint(0,255)))
     tony.setheading(angle)
     tony.forward(20)
 
