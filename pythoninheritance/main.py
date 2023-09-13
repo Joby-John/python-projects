495d6e58f8c0ac5604faa72b4ae3f9691a2eea5e
Joby-John <joby432john@gmail.com>
2023-09-13 08:02:10 +0530

    starting out with inheritance, just writing some comments on how this thing actually works

--- pythoninheritance/main.py
+++ pythoninheritance/main.py
@@ -12,6 +12,10 @@ class Humans(Animals):  # class Humans inherits from class Animals
     def __init__(self):
         super().__init__()  # this line is essential for inheritance to work
 
+    def walk(self):  # to do something special in inheriting classes function use this
+        super().walk()  # this means first super classes method
+        print("Just using 2 legs")  # then from this method
+
 
 roby = Humans()
 
