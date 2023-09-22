4ec64978a5ce519c5dc1da5ef16a00052a65f6df
Joby-John <joby432john@gmail.com>
2023-09-22 21:30:39 +0530

    made code pretty

--- college_works/lambda_functions28/main.py
+++ college_works/lambda_functions28/main.py
@@ -4,10 +4,13 @@ ar_tri = lambda base, height: base * height * .5
 ar_rect = lambda length, breadth: length * breadth
 match choice:
     case 1:
-        print(f'{round(ar_sq(float(input("Enter the length of the side:-"))),2)} sq.units')
+        area = ar_sq(float(input("Enter the length of the side:-")))
+        print(f'{round(area,2)} sq.units')
     case 2:
-        print(f'{round(ar_rect(float(input("Enter length of length:-")), float(input("Enter breadth:-"))),2)} sq.units')
+        area = ar_rect(float(input("Enter length:-")), float(input("Enter breadth:-")))
+        print(f'{round(area,2)} sq.units')
     case 3:
-        print(f'{round(ar_tri(float(input("Enter base:- ")), float(input("Enter height:-"))),2)} sq.units')
+        area = ar_tri(float(input("Enter base:- ")), float(input("Enter height:-")))
+        print(f'{round(area,2)} sq.units')
     case _:
         print("Invalid choice")
