3bd850d4a78c24ee70e15158ff589ded0994553d
Joby-John <joby432john@gmail.com>
2023-09-23 02:17:21 +0530

    made code pretty

--- college_works/char_freq23/main.py
+++ college_works/char_freq23/main.py
@@ -1,7 +1,7 @@
-split_list = [chara for chara in input("Enter a string:-")]
+split_list = [chara for chara in input("Enter a string:-").lower()]
 char_freq = {}
 for letter in split_list:
-    if letter != " ":
+    if letter != " " and letter not in char_freq:
         count = split_list.count(letter)
         char_freq.update({letter: count})# when another value comes for same key it just gets replaced so dont worry on repetition
 print(char_freq)
