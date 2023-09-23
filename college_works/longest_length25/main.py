a99573556d8dc2611b0e03aaf2ec2b329cc02302
Joby-John <joby432john@gmail.com>
2023-09-23 13:46:59 +0530

    made code pretty

--- college_works/longest_length25/main.py
+++ college_works/longest_length25/main.py
@@ -1,6 +1,13 @@
 words = input("Enter a list of word of which you want to find characters separated by ,:- ").split(",")
-biggest = words[0]
+biggest = words[0].strip(" ")
+len_list = []
 for word in words:
+    word = word.strip(" ")
+    len_list.append(len(word))
     if len(biggest) < len(word):
         biggest = word
-print(f"The biggest word is:- {biggest} with {len(biggest)} characters")
+if len_list.count(len(biggest)) > 1:
+    bigs = [word for word in words if len(word.strip(" ")) == len(biggest)]
+    print(f"some words entered are of equal length they are:-{bigs} with {len(biggest)} letters")
+else:
+    print(f"The biggest word is:- {biggest} with {len(biggest)} characters")
